7ac40d424bebf5fe479c9308000952bad26c4df5
𝐒𝐇𝐎𝐍𝐀 𝐁𝐎𝐓𝐒🚩 <harshkumar12038@gmail.com>
2026-06-03 01:30:29 +0530

    Update info.py

--- AloneX/plugins/info.py
+++ AloneX/plugins/info.py
@@ -370,18 +370,10 @@ async def UserInfo(update, context):
     keyboard = None
     try:
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton(f"📋 {full_name}", url=f"tg://user?id={user.id}")]])
-    except:
-        pass
+    
 
     
-            except BadRequest:
-                try:
-                    await message.reply_photo(photo=photo_bytes, caption=text, 
-                                            parse_mode=constants.ParseMode.HTML)
-                    await msg.delete()
-                    return
-                except:
-                    pass
+            
 
     try:
         await msg.edit_text(text=text, parse_mode=constants.ParseMode.HTML, reply_markup=keyboard)
